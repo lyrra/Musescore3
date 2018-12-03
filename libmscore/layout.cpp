lyrra/Musescore3

commit 442caafc3c1804d063b8708c5e2a9f675b9bd13e

fix #277666 Space for instrument name of hidden staff is reserved

libmscore/layout.cpp
@@ -1211,7 +1211,6 @@ void Score::hideEmptyStaves(System* system, bool isFirstSystem)
 
       for (Staff* staff : _staves) {
             SysStaff* ss  = system->staff(staffIdx);
-//            bool oldShow = ss->show();
 
             Staff::HideMode hideMode = staff->hideWhenEmpty();
 
@@ -2982,6 +2981,7 @@ System* Score::collectSystem(LayoutContext& lc)
             if (lc.curMeasure->isMeasure()) {
                   Measure* m = toMeasure(lc.curMeasure);
                   if (firstMeasure) {
+                        hideEmptyStaves(system, lc.firstSystem);
                         system->layoutSystem(minWidth);
                         minWidth += system->leftMargin();
                         if (m->repeatStart()) {
@@ -3117,8 +3117,6 @@ System* Score::collectSystem(LayoutContext& lc)
             qreal w = toMeasure(lc.prevMeasure)->createEndBarLines(true);
             minWidth += w;
             }
-
-      hideEmptyStaves(system, lc.firstSystem);
 
       //-------------------------------------------------------
       //    add system trailer if needed

libmscore/layoutlinear.cpp
@@ -190,7 +190,7 @@ void Score::layoutLinear(bool layoutAll, LayoutContext& lc)
       resetSystems(layoutAll, lc);
 
       collectLinearSystem(lc);
-      hideEmptyStaves(systems().front(), true);
+//      hideEmptyStaves(systems().front(), true);     this does not make sense
 
       lc.layoutLinear();
       }

libmscore/system.cpp
@@ -187,7 +187,6 @@ void System::layoutSystem(qreal xo1)
 
       for (int staffIdx = 0; staffIdx < nstaves; ++staffIdx) {
             Staff* s = score()->staff(staffIdx);
-
             for (int i = 0; i < columns; ++i) {
                   for (auto bi : s->brackets()) {
                         if (bi->column() != i || bi->bracketType() == BracketType::NO_BRACKET)
@@ -237,7 +236,7 @@ void System::layoutSystem(qreal xo1)
                               }
                         }
                   }
-            if (!s->show())
+            if (!staff(staffIdx)->show())
                   continue;
             for (InstrumentName* t : _staves[staffIdx]->instrumentNames) {
                   t->layout();

commit 08055fa755c40cee17797c171002a96e1d251c2e

fix #278057, fix #277666: hide empty staves reusing space for instrument names
The previous fix 442caafc3c1804d063b8708c5e2a9f675b9bd13e
caused a side-effect of hiding non-empty staves if their first
measure was empty.

libmscore/layout.cpp
@@ -2973,6 +2973,7 @@ System* Score::collectSystem(LayoutContext& lc)
       system->setInstrumentNames(lc.startWithLongNames);
 
       qreal minWidth    = 0;
+      qreal layoutSystemMinWidth = 0;
       bool firstMeasure = true;
       bool createHeader = false;
       qreal systemWidth = styleD(Sid::pagePrintableWidth) * DPI;
@@ -2987,7 +2988,7 @@ System* Score::collectSystem(LayoutContext& lc)
             if (lc.curMeasure->isMeasure()) {
                   Measure* m = toMeasure(lc.curMeasure);
                   if (firstMeasure) {
-                        hideEmptyStaves(system, lc.firstSystem);
+                        layoutSystemMinWidth = minWidth;
                         system->layoutSystem(minWidth);
                         minWidth += system->leftMargin();
                         if (m->repeatStart()) {
@@ -3122,6 +3123,22 @@ System* Score::collectSystem(LayoutContext& lc)
       if (lc.prevMeasure && lc.prevMeasure->isMeasure()) {
             qreal w = toMeasure(lc.prevMeasure)->createEndBarLines(true);
             minWidth += w;
+            }
+
+      hideEmptyStaves(system, lc.firstSystem);
+      bool allShown = true;
+      for (const SysStaff* ss : *system->staves()) {
+            if (!ss->show()) {
+                  allShown = false;
+                  break;
+                  }
+            }
+      if (!allShown) {
+            // Relayout system decorations to reuse space properly for
+            // hidden staves' instrument names or other hidden elements.
+            minWidth -= system->leftMargin();
+            system->layoutSystem(layoutSystemMinWidth);
+            minWidth += system->leftMargin();
             }
 
       //-------------------------------------------------------