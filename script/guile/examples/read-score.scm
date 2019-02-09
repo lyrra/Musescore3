d47aaaab1826c761f287264662da5e05469371ef
Larry Valkama <lz64@proton.me>
2019-02-09 22:00:10 +0100

    MSCX to sxml uses scheme modules
    Make it work again by using modules.
    And also take the MSCX file to convert
    from command line.

--- script/guile/examples/read-score.scm
+++ script/guile/examples/read-score.scm
@@ -1,27 +1,30 @@
 
-(use-modules (ice-9 format))
-(primitive-load "script/guile/musescore.scm")
-
-(use-modules (ice-9 textual-ports)) ; get-string-all
-(use-modules (sxml simple))
+(define-module (convert-mscx)
+               #:use-module (ice-9 format)
+               #:use-module (ice-9 textual-ports) ; get-string-all
+               #:use-module (sxml simple)
+               ;#:use-module (lib musescore-c)
+               ;#:use-module (lib musescore)
+               )
 
 ; Open an mscx file, and read all its content (raw XML)
 
-(let ((p (open-input-file "my.mscx")))
+(let ((p (open-input-file (cadr (command-line)))))
   (let ((str (get-string-all p)))
 
     ; send that XML content to a musescore-reader
     ; that will read it and create an internal
     ; score object
-    (let ((score (ms-score-read-string str)))
-      (format #t "score: ~s~%" score)
-      (format #t "  measures: ~s~%" (ms-score-measures score))
-      (for-each (lambda (mea)
-                  (format #t "    measure ~s segments: ~s~%"
-                          mea (ms-measure-segments mea)))
-                (ms-score-measures score)))
+    ;(let ((score (ms-score-read-string str)))
+    ;  (format #t "score: ~s~%" score)
+    ;  (format #t "  measures: ~s~%" (ms-score-measures score))
+    ;  (for-each (lambda (mea)
+    ;              (format #t "    measure ~s segments: ~s~%"
+    ;                      mea (ms-measure-segments mea)))
+    ;            (ms-score-measures score)))
 
     ; Or take that same raw XML content and
     ; convert it to Guile SXML tags:
     (let ((xml (xml->sxml str)))
-      (format #t "~s~%" xml))))
+      (display xml)
+      (newline))))
