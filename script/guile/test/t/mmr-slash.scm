76c748f8de210ef8df7fcabe327ca23dbb21e2cf
Larry Valkama <lz64@proton.me>
2019-02-14 12:28:18 +0100

    mmr slash test script

--- script/guile/test/t/mmr-slash.scm
+++ script/guile/test/t/mmr-slash.scm
@@ -1,3 +1,40 @@
+; This test will select a multimeasure
+; and perform slash-fill action on the
+; selection.
+; Ensure outcome doesn't crash, succeed or not.
 
-(format #t "foobar!~%")
-
+(let ((score (test-read-score-sxlm
+ '(*TOP*
+   (museScore (@ (version "3.01"))
+    (Score
+      (Style (createMultiMeasureRests 1))
+      (Part (Staff (@ (id 1))))
+      (Staff (@ (id 1))
+        (Measure
+          (voice
+            (TimeSig (sigN 4) (sigD 4))
+            (Rest
+              (durationType measure)
+              (duration "4/4"))))
+        (Measure (@ (len "8/4"))
+          (multiMeasureRest 2)
+          (voice
+            (TimeSig (sigN 4) (sigD 4))
+            (Rest
+              (durationType measure)
+              (duration "8/4"))))
+        (Measure
+          (voice
+            (Rest
+              (durationType measure)
+              (duration "4/4")))))))))))
+  (let ((sel (ms-score-selection score)))
+    (ms-selection-setRangeTicks sel 0 1920 0 1)
+    (ms-selection-setStartSegment sel (ms-score-tick2segment score 0))
+    (ms-selection-setEndSegment sel   (ms-score-tick2segment score 1920))
+    (let ((seg (ms-score-tick2segment score 0)))
+      (let ((elmvec (ms-segment-elements seg)))
+        (ms-score-select score (vector-ref elmvec 0) 1 0)))
+    (ms-score-cmd score "slash-fill"))
+  (ms-score-update score)
+  (ms-score-forget score)) ; C++ delete score
