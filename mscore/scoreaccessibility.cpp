lyrra/Musescore3

commit 8f56d6ce70b3e17283bdf98b77446c036606d564

fix #113551 Screen readers: Implement status messages
Creates an 'accessibleMessage' string and demonstrates its use by
providing a status message when switching to note input mode.

The 'accessibleMessage' string is different to the pre-existing
'accessibleInfo' string that provides information about the selected
element(s). Two strings are needed because changes to the 'info' string
are calculated based on its previous value, hence this value must not
be overwritten by other messages.

mscore/scoreaccessibility.cpp
@@ -91,8 +91,13 @@ QString AccessibleScoreView::text(QAccessible::Text t) const
                   //return "";
                   return s->score()->title();
             case QAccessible::Value:
-            case QAccessible::Description:
-                  return s->score()->accessibleInfo();
+            case QAccessible::Description: {
+                  QString msg = s->score()->accessibleMessage();
+                  if (msg.isEmpty())
+                        return s->score()->accessibleInfo();
+                  s->score()->setAccessibleMessage(""); // clear the message
+                  return msg;
+                  }
             default:
                   return QString();
            }
@@ -344,9 +349,26 @@ ScoreAccessibility* ScoreAccessibility::instance()
 void ScoreAccessibility::updateAccessibilityInfo()
       {
       ScoreView* w = static_cast<MuseScore*>(mainWindow)->currentScoreView();
-      if (!w) return;
+      if (!w)
+            return;
 
-      currentInfoChanged();
+      if (w->score()->accessibleMessage().isEmpty())
+            currentInfoChanged();
+
+      // Try to send message to the screen reader. Note that NVDA will
+      // ignore the message if it is the same as the previous message.
+      updateAccessibility();
+
+      // HACK: send the message again after a short delay to force NVDA
+      // to read it even if it is the same as before.
+      QTimer::singleShot(0, this, &ScoreAccessibility::updateAccessibility);
+      }
+
+void ScoreAccessibility::updateAccessibility()
+      {
+      ScoreView* w = static_cast<MuseScore*>(mainWindow)->currentScoreView();
+      if (!w)
+            return;
 
       //getInspector->isAncestorOf is used so that inspector and search dialog don't loose focus
       //when this method is called

libmscore/score.h
@@ -488,7 +488,8 @@ class Score : public QObject, public ScoreElement {
       PlayMode _playMode { PlayMode::SYNTHESIZER };
 
       qreal _noteHeadWidth { 0.0 };       // cached value
-      QString accInfo;                    ///< information used by the screen-reader
+      QString accInfo;                    ///< information about selected element(s) for use by screen-readers
+      QString accMessage;                 ///< temporary status message for use by screen-readers
 
       //------------------
 
@@ -1186,6 +1187,9 @@ class Score : public QObject, public ScoreElement {
 
       void setAccessibleInfo(QString s)   { accInfo = s.remove(":").remove(";"); }
       QString accessibleInfo() const      { return accInfo;          }
+
+      void setAccessibleMessage(QString s) { accMessage = s.remove(":").remove(";"); }
+      QString accessibleMessage() const    { return accMessage; }
 
       QImage createThumbnail();
       QString createRehearsalMarkText(RehearsalMark* current) const;

mscore/musescore.cpp
@@ -4239,6 +4239,8 @@ void MuseScore::inputMethodVisibleChanged()
 
 void MuseScore::showModeText(const QString& s)
       {
+      if (cs)
+            cs->setAccessibleMessage(s);
       _modeText->setText(s);
       _modeText->show();
       }

mscore/scoreaccessibility.h
@@ -87,6 +87,9 @@ class ScoreAccessibility : public QObject {
       static ScoreAccessibility* instance();
       void currentInfoChanged();
       static void makeReadable(QString&);
+
+   private slots:
+      void updateAccessibility();
       };
 
 }

commit 64bc5f8c456910f11a1509a52f3fb67f108aef15

fix #306633: Always update status bar text
Fixes regression caused by commit 8f56d6c in PR #6173.

The accessibleMessage string is cleared in the function
AccessibleScoreView::text(), but that function only gets called if
there is a screen reader running. If there is no screen reader then
accessibleMessage would never be emptied after it was first set, hence
currentInfoChanged() was never called to set the status bar text.

mscore/scoreaccessibility.cpp
@@ -352,16 +352,19 @@ void ScoreAccessibility::updateAccessibilityInfo()
       if (!w)
             return;
 
-      if (w->score()->accessibleMessage().isEmpty())
-            currentInfoChanged();
+      currentInfoChanged();
 
       // Try to send message to the screen reader. Note that NVDA will
       // ignore the message if it is the same as the previous message.
       updateAccessibility();
 
+#if defined(Q_OS_WIN)
       // HACK: send the message again after a short delay to force NVDA
-      // to read it even if it is the same as before.
+      // to read it even if it is the same as before. This is useful when
+      // cursoring through a word with repeated characters, such as "food".
+      // Without this hack NVDA would say "f", "o", *silence*, "d".
       QTimer::singleShot(0, this, &ScoreAccessibility::updateAccessibility);
+#endif
       }
 
 void ScoreAccessibility::updateAccessibility()