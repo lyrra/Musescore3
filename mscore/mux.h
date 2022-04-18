36afe53f819f1eb779248b824cc36243874d9b43
Larry Valkama <lz64@proton.me>
2022-04-18 08:05:06 +0200

    copyright

--- mscore/mux.h
+++ mscore/mux.h
@@ -1,3 +1,8 @@
+/* GPL-2.0-or-later
+ * Copyright (C) 2022 Larry Valkama
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
+ */
 
 #ifndef __MUX_H__
 #define __MUX_H__
