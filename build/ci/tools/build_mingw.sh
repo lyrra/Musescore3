a9c7c65655f0d3e97ef1956408cf6468a0130e3c
Larry Valkama <lz64@proton.me>
2022-04-30 23:43:26 +0200

    cmake no verbose -D flag

--- build/ci/tools/build_mingw.sh
+++ build/ci/tools/build_mingw.sh
@@ -15,7 +15,6 @@ command -v cmake
 cmake -G "MinGW Makefiles" \
       -DCMAKE_INSTALL_PREFIX=../win32install \
       -DCMAKE_BUILD_TYPE=DEBUG \
-      -DVERBOSE=1 \
       -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
       -DCMAKE_MAKE_PROGRAM=mingw32-make.exe \
       -DBUILD_FOR_WINSTORE=OFF \
