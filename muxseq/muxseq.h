479e2333f652e40d7fb04fbbb151ec48195d127b
Larry Valkama <lz64@proton.me>
2022-05-30 22:11:37 +0200

    fix muxseq header define

--- muxseq/muxseq.h
+++ muxseq/muxseq.h
@@ -4,8 +4,8 @@
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
  */
 
-#ifndef __MUX_H__
-#define __MUX_H__
+#ifndef __MUXSEQ_H__
+#define __MUXSEQ_H__
 
 namespace Ms {
 
