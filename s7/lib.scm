91b42f773f69a6e1fd408e867bd1e9e74f2c16f8
Larry Valkama <lz64@proton.me>
2022-07-21 18:45:43 +0200

    scheme functions
    * misc lisp functions
      match (very primitive), remove-if, assq-ref
      assis?, assoc-set

---
+++ s7/lib.scm
@@ -0,0 +1,48 @@
+(define-macro (match expr case1)
+  (let ((lst (car case1))
+        (body (cdr case1)))
+  `(let ((ex ,expr))
+     (define (flatten lst)
+       (cond ((null? lst) ())
+             ((pair? lst)
+              (if (pair? (car lst))
+                  (append (flatten (car lst)) (flatten (cdr lst)))
+                  (cons (car lst) (flatten (cdr lst)))))
+             (#t lst)))
+
+     (define (structures-equal? l1 l2)
+       (if (pair? l1)
+           (and (pair? l2)
+                (structures-equal? (car l1) (car l2))
+                (structures-equal? (cdr l1) (cdr l2)))
+           (not (pair? l2))))
+
+     (if (not (structures-equal? ',lst ex))
+         (error 'test-error "~A and ~A do not match" ',lst ex))
+
+     (let ((names (flatten ',lst))
+           (vals (flatten ex)))
+       (apply (eval (list 'lambda names '(begin ,@body))) vals)))))
+
+(define (assq-ref lst key)
+  (if (assoc key lst)
+      (cdr (assoc key lst))
+      #f))
+
+(define (remove-if func lst)
+  (map (lambda (x) (if (func x) (values) x)) lst))
+
+(define (assis? key lst)
+  (let ((pair (assoc key lst)))
+    (if pair
+        (cdr pair)
+        #f)))
+
+(define (assoc-set lst key val)
+  (let ((pair (assq key lst)))
+    (cond
+     (pair
+      (set-cdr! pair val)
+      lst)
+     (else
+      (cons (cons key val) lst)))))
